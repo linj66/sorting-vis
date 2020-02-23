8b20b530f1d838a888dfbac20e600cce8e1d9c93
Jerry Lin <linj1064@gmail.com>
2020-02-22 21:17:52 -0800

    timsort complete sans galloping

--- sorting_algos.py
+++ sorting_algos.py
@@ -393,8 +393,41 @@ def introsort(xs, insertion_threshold=16):  # https://en.wikipedia.org/wiki/Intr
 def timsort(xs):  
     # https://en.wikipedia.org/wiki/Timsort
     # https://github.com/python/cpython/blob/master/Objects/listsort.txt
-    #if len(xs) <= 64:
-    #    yield from binsort(xs)
+
+    def binary_search(xs, target, start, end):
+        if start >= end:
+            if start > end or xs[start] > target:
+                yield xs + [[start]]
+                return start
+            else:
+                yield xs + [[start + 1]]
+                return start + 1
+            yield xs + [[start, end]]
+
+        mid = (start + end) // 2
+
+        yield xs + [[mid]]
+        if xs[mid] < target:
+            result = yield from binary_search(xs, target, mid + 1, end)
+            return result
+        elif xs[mid] > target:
+            result = yield from binary_search(xs, target, start, mid - 1)
+            return result
+        else:
+            return mid
+        
+    def binsort(xs, lo, hi):  # insertion sort routine 
+        for i in range(lo, hi):
+            print(i, lo, i - 1, hi)
+            swap = yield from binary_search(xs, xs[i], lo, i - 1)
+            yield xs + [[i, swap]]
+            xs = xs[:swap] + [xs[i]] + xs[swap:i] + xs[i + 1:]
+        yield xs + [[]]
+        return xs
+
+    if len(xs) <= 64:
+        yield from binsort(xs, 0, len(xs))
+        return
     
     def find_minrun():
         length = len(xs)
@@ -407,26 +440,31 @@ def timsort(xs):
 
     def count_run(xs, start):  # finds length of next run, swaps if decreasing
         if start >= len(xs):
+            yield xs
             return 0
         elif start == len(xs) - 1:
+            yield xs + [[len(xs) - 1]]
             return 1
 
         curr = start
         if xs[curr + 1] < xs[curr]:
+            yield xs + [[curr, curr + 1]]
             curr += 1
             while curr < len(xs) - 1 and xs[curr + 1] < xs[curr]:
+                yield xs + [[curr, curr + 1]]
                 curr += 1
             i = start
             j = curr
             while i < j:
                 xs[i], xs[j] = xs[j], xs[i]
-                # yield
+                yield xs + [[i, j]]
                 i += 1 
                 j -= 1
             return curr - start + 1
         else:
             curr += 1
             while curr < len(xs) - 1 and xs[curr + 1] >= xs[curr]:
+                yield xs + [[curr, curr + 1]]
                 curr += 1
             return curr - start + 1
 
@@ -438,22 +476,26 @@ def timsort(xs):
                 b = s[-2]
                 c = s[-1]
                 if a[1] <= b[1] + c[1] or b[1] <= c[1]:
+                    print("merging", a, b, c)
                     if a[1] <= b[1] + c[1]:
                         if a[1] > c[1]:
                             start = min(b[0], c[0])
-                            xs[start:start + b[1] + c[1]] = merge(xs[b[0]:b[0] + b[1]], xs[c[0]:c[0] + c[1]])
+                            #xs[start:start + b[1] + c[1]] = merge(xs[b[0]:b[0] + b[1]], xs[c[0]:c[0] + c[1]])
+                            yield from merge(xs, b, c)
                             s.pop()
                             s.pop()
                             s.append((start, b[1] + c[1]))
                         else:  # going to need to update stack some time
                             start = min(a[0], b[0])
-                            xs[start:start + b[1] + a[1]] = merge(xs[b[0]:b[0] + b[1]], xs[a[0]:a[0] + a[1]])
+                            #xs[start:start + b[1] + a[1]] = merge(xs[b[0]:b[0] + b[1]], xs[a[0]:a[0] + a[1]])
+                            yield from merge(xs, a, b)
                             s.pop(-3)
                             s.pop(-2)
                             s.append((start, a[1] + b[1]))
                     else:
                         start = min(b[0], c[0])
-                        xs[start:start + b[1] + c[1]] = merge(xs[b[0]:b[0] + b[1]], xs[c[0]:c[0] + c[1]])
+                        #xs[start:start + b[1] + c[1]] = merge(xs[b[0]:b[0] + b[1]], xs[c[0]:c[0] + c[1]])
+                        yield from merge(xs, b, c)
                         s.pop()
                         s.pop()
                         s.append((start, b[1] + c[1]))
@@ -465,89 +507,99 @@ def timsort(xs):
                 b = s[-2]
                 c = s[-1]
                 if b[1] <= c[1]:
-                    print(b, c)
+                    print("merging:", b, c)
                     start = min(b[0], c[0])
-                    xs[start:start + b[1] + c[1]] = merge(xs[b[0]:b[0] + b[1]], xs[c[0]:c[0] + c[1]])
+                    #xs[start:start + b[1] + c[1]] = merge(xs[b[0]:b[0] + b[1]], xs[c[0]:c[0] + c[1]])
+                    yield from merge(xs, b, c)
                     s.pop()
                     s.pop()
                     s.append((start, b[1] + c[1]))
             break
 
-    def binary_search(xs, target, start, end):  # find extremities
-        if start >= end:
-            if start > end or xs[start] > target:
-                return start
-            else:
-                return start + 1
+    def merge(xs, small_tup, big_tup):  # merge runs from stack together
+        if big_tup[1] < small_tup[1]:
+            small_tup, big_tup = big_tup, small_tup
 
-        mid = (start + end) // 2
-        if xs[mid] < target:
-            return binary_search(xs, target, mid + 1, end)
-        elif xs[mid] > target:
-            return binary_search(xs, target, start, mid - 1)
-        else:
-            return mid
+        sm_start = small_tup[0]
+        sm_end = small_tup[1] + sm_start
+        bg_start = big_tup[0]
+        bg_end = big_tup[1] + bg_start
 
-    def merge(small, big):  # merge runs from stack together
-        if len(big) < len(small):
-            small, big = big, small
-        first_big_in_small = binary_search(small, big[0], 0, len(small) - 1)
-        last_small_in_big = binary_search(big, small[-1], 0, len(big) - 1)
-        temp = small[first_big_in_small:].copy()
-        #print("fbins", first_big_in_small)
-        #print("lsinb", last_small_in_big)
+        small = xs[sm_start:sm_end]
+        big = xs[bg_start:bg_end]
+
+        fbins = yield from binary_search(xs, big[0], sm_start, sm_end - 1)
+        fbins -= sm_start
+        lsinb = yield from binary_search(xs, small[-1], bg_start, bg_end - 1)
+        lsinb -= bg_start
+        temp = small[fbins:].copy()
+        #print("fbins", fbins)
+        #print("lsinb", lsinb)
         #print(small, big)
         #print(temp)
         #print()
         i = 0
         j = 0
-        idx = first_big_in_small
-        while i < len(temp) and j <= min(last_small_in_big, len(big) - 1):
+        idx = fbins
+        while i < len(temp) and j <= min(lsinb, len(big) - 1):
             #print(big[j], "vs.", temp[i])
             if big[j] < temp[i]:
                 if idx >= len(small):
                     #print("big[%s] = %s" % (idx - len(small), big[j]))
                     big[idx - len(small)] = big[j]
+                    # yield
                 else:
                     #print("small[%s] = %s" % (idx, big[j]))
                     small[idx] = big[j]
+                    # yield
                 j += 1
             else: # if temp[i] <= big[j]; i believe this maintains stability?
                 if idx >= len(small):
                     #print("big[%s] = %s" % ((idx - len(small), temp[i])))
                     big[idx - len(small)] = temp[i]
+                    # yield
                 else:
                     #print("small[%s] = %s" % (idx, temp[i]))
                     small[idx] = temp[i]
+                    # yield
                 i += 1
             idx += 1
         #print(small, big)
         if i < len(temp):
             big[-(len(temp) - i):] = temp[i:]
-        return small + big
+            # yield
+        #return small + big
+        xs_idx = min(small_tup[0], big_tup[0])
+        for val in small + big:
+            xs[xs_idx] = val
+            yield xs + [[xs_idx]]
+            xs_idx += 1
 
-    #minrun = find_minrun()
+    minrun = find_minrun()
     stack = list()
 
     # here we go
     idx = 0
     while idx < len(xs):
-        len_run = count_run(xs, idx)
-        #if len_run < minrun:
-        #    yield from binsort(xs, idx, idx + minrun)
-        #    len_run = minrun
+        len_run = yield from count_run(xs, idx)
+        if len_run < minrun:
+            print("run too small")
+            xs = yield from binsort(xs, idx, min(idx + minrun, len(xs)))
+            len_run = min(minrun, len(xs) - idx)
         stack.append((idx, len_run))
         idx += len_run
-        merge_collapse(xs, stack)
+        yield from merge_collapse(xs, stack)
     print("end of stepping", stack)
     while len(stack) > 1:
         a = stack.pop()
         b = stack.pop()
         start = min(a[0], b[0])
-        xs[start:start + a[1] + b[1]] = merge(xs[a[0]:a[0] + a[1]], xs[b[0]:b[0] + b[1]])
-        stack.append((start, start + a[1] + b[1]))
+        #xs[start:start + a[1] + b[1]] = merge(xs[a[0]:a[0] + a[1]], xs[b[0]:b[0] + b[1]])
+        yield from merge(xs, a, b)
+        stack.append((start, a[1] + b[1]))
+        print(stack)
     print(stack)
-    return xs
+    yield xs + [[]]
 
         
         
